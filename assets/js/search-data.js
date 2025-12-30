754196e58b91c7a24c04b79250341c3c15dccfb0
AdamAufdi <117049265+AdamAufdi@users.noreply.github.com>
2025-12-30 21:53:39 +0000

    Deploying to gh-pages from @ AdamAufdi/AdamAufdi.github.io@80f3f408018a2ae84ff3283776df04d222eb4157 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,14 +39,14 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-komatsu-sdv",
+          title: 'Komatsu SDV',
+          description: "Software Defined Vehicle and modular sensor testbed for perception and autonomy",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-automated-visual-navigation-testing-pipeline",
+          title: 'Automated Visual Navigation Testing Pipeline',
+          description: "MATLAB–Python–Blender pipeline for rendering, data association, and evaluation of visual navigation algorithms",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
