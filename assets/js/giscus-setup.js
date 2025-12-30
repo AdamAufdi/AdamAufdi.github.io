b64051483a9d96eb15ea6c925b3f96c0a5a7a920
AdamAufdi <117049265+AdamAufdi@users.noreply.github.com>
2025-12-30 19:07:47 +0000

    Deploying to gh-pages from @ AdamAufdi/AdamAufdi.github.io@def8c55d5054e25d35fc5b9a8c4a9409ac703cf9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -1,15 +1,6 @@
 function determineGiscusTheme() {
   
-    let theme =
-      localStorage.getItem("theme") ||
-      document.documentElement.getAttribute("data-theme") ||
-      "system";
-
-    if (theme === "dark") return "";
-    if (theme === "light") return "";
-
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return prefersDark ? "" : "";
+    return "dark";
   
 }
 
@@ -20,15 +11,15 @@ function determineGiscusTheme() {
     src: "https://giscus.app/client.js",
     "data-repo": "AdamAufdi/AdamAufdi.github.io",
     "data-repo-id": "",
-    "data-category": "",
+    "data-category": "Comments",
     "data-category-id": "",
-    "data-mapping": "",
-    "data-strict": "",
-    "data-reactions-enabled": "",
-    "data-emit-metadata": "",
-    "data-input-position": "",
+    "data-mapping": "title",
+    "data-strict": "1",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
     "data-theme": giscusTheme,
-    "data-lang": "",
+    "data-lang": "en",
     crossorigin: "anonymous",
     async: true,
   };
