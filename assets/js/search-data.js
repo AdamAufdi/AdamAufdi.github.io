4ee93c5efc26d492d2e109364d9354f51edda741
AdamAufdi <117049265+AdamAufdi@users.noreply.github.com>
2025-12-30 20:52:58 +0000

    Deploying to gh-pages from @ AdamAufdi/AdamAufdi.github.io@d93878728b23b694f8227a70dfe3c5b7755ba1c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "Cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "This is my resume. Updated as of 12/30/25",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
